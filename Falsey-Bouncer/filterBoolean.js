75d7e680dbb075d33feb6f3219857d3066a42dcc
Sandra Marie <smarie.codepro@gmail.com>
2016-08-30 13:58:18 -0400

    Update filterBoolean.js

--- Falsey-Bouncer/filterBoolean.js
+++ Falsey-Bouncer/filterBoolean.js
@@ -3,13 +3,7 @@
    Remember to use Read-Search-Ask if you get stuck. Write your own code.
    Here are some helpful links:
    Boolean Objects
-   Array.prototype.filter()
-
-Given data:
-bouncer([7, "ate", "", false, 9]) should return [7, "ate", 9].
-bouncer(["a", "b", "c"]) should return ["a", "b", "c"].
-bouncer([false, null, 0, NaN, undefined, ""]) should return [].
-bouncer([1, null, NaN, 2, undefined]) should return [1, 2]. */
+   Array.prototype.filter() */
 
   function bouncer(arr) {
 
@@ -19,3 +13,9 @@ bouncer([1, null, NaN, 2, undefined]) should return [1, 2]. */
     return Boolean(newArr); //using Boolean will return only true array elements
     }
 }
+
+/* Given data:
+bouncer([7, "ate", "", false, 9]) should return [7, "ate", 9].
+bouncer(["a", "b", "c"]) should return ["a", "b", "c"].
+bouncer([false, null, 0, NaN, undefined, ""]) should return [].
+bouncer([1, null, NaN, 2, undefined]) should return [1, 2]. */
